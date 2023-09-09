83b8d75de90f80cbaab723ff2e62caae9495f6ab
edcastillob <edwar.castillo@gmail.com>
2023-09-08 23:09:32 -0400

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const { postSurvey } = require("../controllers/postSurvey");
+const { getSurvey } = require("../controllers/getSurvey");
+const { putSurvey } = require("../controllers/putSurvey");
+const { getSurveyId } = require("../controllers/getSurveyId");
+const { deleteSurvey } = require("../controllers/deleteSurvey");
+
+const router = Router();
+
+
+router.get('/', function(req, res) {
+    res.send('Backend Survey');
+  })
+
+  router.post("/survey", postSurvey);
+  router.get("/survey", getSurvey);
+  router.put("/survey/:id", putSurvey);
+  router.get("/survey/:id", getSurveyId);
+  router.delete("/survey/:id", deleteSurvey);
+
+
+
+module.exports = router;
